18c57b0b08b9a07560dfc85290e154598373516b
Chirag <chirag.sharma.gov2005@gmail.com>
2026-09-11 10:08:04 +0530

    form fix

--- src/components/layout/Contact.tsx
+++ src/components/layout/Contact.tsx
@@ -17,7 +17,7 @@ export default function Contact() {
     const formData = new FormData(form);
 
     try {
-      await fetch("/", {
+      await fetch("/__forms.html", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: new URLSearchParams(formData as any).toString(),
